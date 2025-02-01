58145ab77bc4e4ee1bf496a0003670da2e25b15a
Justin Zandbergen <justin.zandbergen@topicus.nl>
2025-02-01 19:27:58 +0100

    feat: added color and flags

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"log"
@@ -20,19 +21,101 @@ var (
 	Commit    string
 )
 
-// id:name
+type gender bool
+
+const (
+	female gender = true
+	male   gender = false
+)
+
+const (
+	Reset  = "\033[0m"
+	Red    = "\033[31m"
+	Green  = "\033[32m"
+	Yellow = "\033[33m"
+	Blue   = "\033[34m"
+	Purple = "\033[35m"
+	Cyan   = "\033[36m"
+	Gray   = "\033[37m"
+	Dark   = "\033[90m" // Dark gray
+)
+
 type uuidMap map[string]string
 
 var thisMap = make(uuidMap, 32)
 
+type nameConfig struct {
+	gender    string
+	useTitle  bool
+	useFirst  bool
+	useLast   bool
+	formatStr string
+	color     bool
+}
+
+var config nameConfig
+var currentGender gender
+
+func init() {
+	flag.StringVar(&config.gender, "gender", "both", "Gender for name generation (male/female/both)")
+	flag.BoolVar(&config.useTitle, "title", false, "Include title in name")
+	flag.BoolVar(&config.useFirst, "first", true, "Include first name")
+	flag.BoolVar(&config.useLast, "last", true, "Include last name")
+	flag.StringVar(&config.formatStr, "format", "[%s]", "Format string for the name output")
+	flag.BoolVar(&config.color, "color", true, "Use colors")
+}
+
 func (m uuidMap) Run(s string) (string, error) {
 	for uuid, name := range m {
-		s = strings.ReplaceAll(s, uuid, name)
+		if config.color {
+			s = strings.ReplaceAll(s, uuid, Dark+name+Reset)
+		} else {
+			s = strings.ReplaceAll(s, uuid, name)
+		}
 	}
 	return s, nil
 }
 
+func generateName() string {
+	var parts []string
+	if config.gender == "both" {
+		currentGender = !currentGender
+	}
+
+	if config.useTitle {
+		if currentGender == female {
+			parts = append(parts, faker.TitleFemale())
+		} else {
+			parts = append(parts, faker.TitleMale())
+		}
+	}
+
+	if config.useFirst {
+		if currentGender == female {
+			parts = append(parts, faker.FirstNameFemale())
+		} else {
+			parts = append(parts, faker.FirstNameMale())
+		}
+	}
+
+	if config.useLast {
+		parts = append(parts, faker.LastName())
+	}
+
+	return fmt.Sprintf(config.formatStr, strings.Join(parts, " "))
+}
+
 func main() {
+	flag.Parse()
+	switch config.gender {
+	case "male":
+		currentGender = male
+	case "female":
+		currentGender = female
+	case "both":
+		currentGender = female
+	}
+
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs,
 		syscall.SIGHUP,
@@ -79,15 +162,8 @@ func processLine(l string) (result string, err error) {
 	}
 	uuids := pattern.FindAllString(l, -1)
 	for _, uuid := range uuids {
-		var name string
-		name, ok := thisMap[uuid]
-		if !ok {
-			// TODO function
-			name = fmt.Sprintf("[UUID: %s %s %s]",
-				faker.TitleMale(),
-				faker.FirstNameMale(),
-				faker.LastName())
-			thisMap[uuid] = name
+		if _, ok := thisMap[uuid]; !ok {
+			thisMap[uuid] = generateName()
 		}
 	}
 
